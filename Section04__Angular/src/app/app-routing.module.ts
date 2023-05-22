f14510bf77022a3ba68743398d89c1ff60141ff0
Nestea Turner <nestor.quiroga@sofka.com.co>
2023-05-21 21:50:15 -0500

    Adding new routes with navbar

--- Section04__Angular/src/app/app-routing.module.ts
+++ Section04__Angular/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import { CounterComponent } from './counter/components/counter/counter.component';
+import { DirectivesComponent } from './directives/directives.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent,  pathMatch: 'full'},
+  { path: 'counter', component: CounterComponent, pathMatch: 'full'},
+  { path: 'directives', component: DirectivesComponent, pathMatch: 'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
